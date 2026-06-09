c3780bf6fb4f6514a13e3701885534a68db2a620
Keven Allan <kevenallan98@gmail.com>
2026-06-08 21:30:36 -0300

    UPDATE-Task-X - Adiciona form de login na tela

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,20 +1,51 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
-  selector: 'app-login',
-  imports: [ButtonModule, FormsModule, InputTextModule, IconFieldModule, InputIconModule, PasswordModule],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+    selector: 'app-login',
+    imports: [
+        ButtonModule,
+        FormsModule,
+        InputTextModule,
+        IconFieldModule,
+        InputIconModule,
+        PasswordModule,
+        DividerModule,
+        ReactiveFormsModule,
+    ],
+    templateUrl: './login.html',
+    styleUrl: './login.css',
 })
 export class Login {
-    email = '';
-    senha = '';
-}
+    form!: FormGroup;
 
+    constructor() {
+        this.configurarFormulario();
+    }
+
+    configurarFormulario() {
+        this.form = new FormBuilder().group({
+            email: ['', [Validators.required, Validators.email]],
+            senha: ['', [Validators.required, Validators.minLength(3)]],
+        });
+    }
+
+    entrar() {
+        if (this.form.invalid) {
+            window.alert("'-'");
+        }
+    }
+}
