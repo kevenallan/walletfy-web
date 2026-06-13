d1e588dd533f1d535fb65eb6fc0db665617baca5
Keven Allan <kevenallan98@gmail.com>
2026-06-13 16:47:23 -0300

    UPDATE-Task-26 - Adicionando @for para montar os cards de forma dinamica

--- src/app/features/gasto/components/card/card.ts
+++ src/app/features/gasto/components/card/card.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
     templateUrl: './card.html',
     styleUrl: './card.css',
 })
-export class Card {}
+export class Card {
+    cards = [
+        {
+            icone: '',
+            corIcone: '',
+            backgroundIcone: '',
+            //DTO
+            titulo: 'Receita',
+            valor: 1000,
+            valorPorcentagemDescricao: -5,
+            //
+            descricao: '',
+        },
+        {
+            titulo: 'Despesas',
+            valor: 1500,
+            valorPorcentagemDescricao: 15,
+        },
+        {
+            titulo: 'Saldo Atual',
+            valor: 750,
+            valorPorcentagemDescricao: -20,
+        },
+        {
+            titulo: 'Pendentes',
+            valor: 375,
+            valorPorcentagemDescricao: 1,
+        },
+    ];
+}
