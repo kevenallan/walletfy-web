c0e8a94a095c83a5386e5290cfb3ef72a5580519
Keven Allan <kevenallan98@gmail.com>
2026-06-28 01:00:50 -0300

    UPDATE-Task-84 - Atualiza o ngOnInit do menu para quando a tela for atualizada adicionar o hover no menu corretamente

--- src/app/layout/menu/menu.ts
+++ src/app/layout/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, output } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { DrawerModule } from 'primeng/drawer';
 import { AuthService } from '../../features/auth/services/auth';
 
@@ -15,11 +15,13 @@ export class Menu implements OnInit {
 
     nomeUsuario!: string;
     rotaAtual = 'gasto';
+    private _router = inject(Router);
 
     private _authService = inject(AuthService);
 
     ngOnInit(): void {
         this.nomeUsuario = this._authService.usuario()?.nome || '';
+        this.rotaAtual = this._router.url.replace('/', '');
     }
 
     selecionarMenu(menu: string) {
