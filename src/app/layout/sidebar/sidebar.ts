8510a950857f07217c4a6ea99d5feaaf4d6bc7d4
Keven Allan <kevenallan98@gmail.com>
2026-07-03 14:38:03 -0300

    UPDATE-Task-118 - Atualiza o menu-mobile para fechar o menu quando for feita alguma navegação entre telas

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -3,7 +3,8 @@ import { Menu } from '../menu/menu';
 import { Drawer } from 'primeng/drawer';
 import { MenuMobile } from '../menu-mobile/menu-mobile';
 import { AuthService } from '../../features/auth/services/auth';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-sidebar',
@@ -16,6 +17,14 @@ export class Sidebar {
 
     private _authService = inject(AuthService);
     private _router = inject(Router);
+
+    constructor() {
+        this._router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe(() => {
+                this.visible = false;
+            });
+    }
 
     sair() {
         this._authService.logout();
